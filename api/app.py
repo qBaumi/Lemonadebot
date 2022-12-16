588ea877104347dab5cbb9adcbb141716dccf6a1
qBaumi <max05posch@gmail.com>
2022-12-16 19:00:03 +0100

    other routes

--- api/app.py
+++ api/app.py
@@ -1,3 +1,4 @@
+import datetime
 import json
 
 import flask
@@ -6,7 +7,7 @@ import sys
 
 # appending the parent directory path
 sys.path.append('../')
-import dbutils
+import dbutils, utils
 
 app = Flask(__name__)
 
@@ -21,7 +22,31 @@ def commands():
 
 @app.route('/emotes')
 def emotes():
-    emotestats = dbutils.sql_select(f"SELECT * FROM emote_tracker")
+    emotestats = dbutils.sql_select(f"SELECT name, count FROM emote_tracker")
+    response = flask.jsonify(emotestats)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+@app.route('/emotes/today')
+def emotes():
+    date = utils.getDate()
+    emotestats = dbutils.sql_select(f"SELECT name, count FROM emote_tracker WHERE date == {date}")
+    response = flask.jsonify(emotestats)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+@app.route('/emotes/monthly')
+def emotes():
+    date = datetime.datetime.fromtimestamp((datetime.datetime.now().timestamp() - 2630000)).strftime("%Y-%m-%d")
+    emotestats = dbutils.sql_select(f"SELECT name, count FROM emote_tracker WHERE CONCAT(SUBSTRING(date, 7, 10), '-', SUBSTRING(date, 4, 2), '-', SUBSTRING(date, 1, 2)) >= '{date}'")
+    response = flask.jsonify(emotestats)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+@app.route('/emotes/weekly')
+def emotes():
+    date = datetime.datetime.fromtimestamp((datetime.datetime.now().timestamp() - 604800)).strftime("%Y-%m-%d")
+    emotestats = dbutils.sql_select(f"SELECT name, count FROM emote_tracker WHERE CONCAT(SUBSTRING(date, 7, 10), '-', SUBSTRING(date, 4, 2), '-', SUBSTRING(date, 1, 2)) >= '{date}'")
     response = flask.jsonify(emotestats)
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
