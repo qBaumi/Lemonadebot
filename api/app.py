366ea196106fe9749112e4155fe35e9b8751f7c3
qBaumi <max05posch@gmail.com>
2022-12-16 19:15:43 +0100

    cors

--- api/app.py
+++ api/app.py
@@ -4,24 +4,15 @@ import json
 import flask
 from flask import Flask
 import sys
+from flask_cors import CORS
 
 # appending the parent directory path
 sys.path.append('../')
 import dbutils, utils
 
 app = Flask(__name__)
+CORS(app) # This will enable CORS for all routes
 
-from flask import Blueprint
-blueprint = Blueprint('blueprint', __name__)
-
-# put this sippet ahead of all your bluprints
-# blueprint can also be app~~
-@blueprint.after_request
-def after_request(response):
-    header = response.headers
-    header['Access-Control-Allow-Origin'] = '*'
-    # Other headers can be added here if needed
-    return response
 
 @app.route('/commands')
 def commands():
