ace9f2e212007ca0384a7493ba61b30d775b58a5
qBaumi <max05posch@gmail.com>
2022-12-16 19:14:00 +0100

    test headers

--- api/app.py
+++ api/app.py
@@ -11,6 +11,17 @@ import dbutils, utils
 
 app = Flask(__name__)
 
+from flask import Blueprint
+blueprint = Blueprint('blueprint', __name__)
+
+# put this sippet ahead of all your bluprints
+# blueprint can also be app~~
+@blueprint.after_request
+def after_request(response):
+    header = response.headers
+    header['Access-Control-Allow-Origin'] = '*'
+    # Other headers can be added here if needed
+    return response
 
 @app.route('/commands')
 def commands():
